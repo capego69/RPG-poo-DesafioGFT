7075d6bcc54850637f5c5929412760464c742bd2
capego69 <capego@hotmail.com>
2022-01-30 16:26:55 -0300

    inicio commit rpg

---
+++ Mentoria/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Mentoria.src.Entities;
+
+namespace Mentoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heroi arus = new Heroi("Arus","Guerreiro");
+            Mago wedge = new Mago("Wedge","Mago Negro");
+            Inimigo KingMummy = new Inimigo("King Mummy", "Zumbi");
+
+            System.Console.WriteLine(arus.Atacar());
+            System.Console.WriteLine(KingMummy.Atacar());
+            
+            if(arus.ValorUltimoAtaque == KingMummy.ValorUltimoAtaque)
+            {
+                System.Console.WriteLine("Empate, ambos deram dano de " + arus.ValorUltimoAtaque);
+            }else if(arus.ValorUltimoAtaque > KingMummy.ValorUltimoAtaque){
+                KingMummy.ReceberDano(arus.ValorUltimoAtaque - KingMummy.ValorUltimoAtaque);
+                System.Console.WriteLine(arus.Nome + " Venceu esse round! :))");
+            }else{
+                arus.ReceberDano(KingMummy.ValorUltimoAtaque - arus.ValorUltimoAtaque);
+                System.Console.WriteLine(KingMummy.Nome + " Venceu esse round :((");
+            }
+
+            System.Console.WriteLine(arus.ValorUltimoAtaque);
+            System.Console.WriteLine(KingMummy.ValorUltimoAtaque);
+        }
+    }
+}
---
+++ Mentoria/src/Entities/Inimigo.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mentoria.src.Entities
+{
+    public class Inimigo : Heroi
+    {
+        public Inimigo(string Nome, string ClasseFantastica) : base(Nome, ClasseFantastica)
+        {
+            this.Nome = Nome;
+            this.ClasseFantastica = ClasseFantastica;
+            this.Nivel = 1;
+            this.PontosDeVida = 80;
+            this.PontosDeMagia = 10;
+        }
+
+        public override string Atacar()
+        {
+            Random dado = new Random();
+            int forcaDoAtaque = this.Nivel + dado.Next(1,15);
+            this.ValorUltimoAtaque = forcaDoAtaque;
+            return this.Nome + "Ataca e da " + forcaDoAtaque + " de dano";
+        }
+    }
+}
